79f774dd179bead411e9cfb2f814048d95fef9de
Jordi <jotolo1252@gmail.com>
2025-01-22 12:38:38 +0100

    añadiendo funcionalidades profe 1

--- crear_activitat.php
+++ crear_activitat.php
@@ -6,19 +6,84 @@ $clau = "";
 $bbdd = "micro2";
 $connexio = mysqli_connect($servidor, $usuari, $clau, $bbdd);
 
-// Verificar la sesión del usuario (nombre del profesor)
-// Supongamos que el nombre del profesor está almacenado en una variable de sesión llamada 'nombre_profesor'
+// Verificar la sesión del usuario
 session_start();
 $nombreProfesor = $_SESSION['nombre_profesor'] ?? 'Desconocido'; // Si no hay sesión, mostrar 'Desconocido'
+
+// Mensaje para mostrar los resultados
+$mensaje = "";
+
+// Manejo de las acciones de las actividades
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Crear actividad
+if (isset($_POST['crear_activitat'])) {
+    $id_activitat = mysqli_real_escape_string($connexio, $_POST['id_activitat'] ?? '');
+    $nom_activitat = mysqli_real_escape_string($connexio, $_POST['nom_activitat'] ?? '');
+    $data_inici = mysqli_real_escape_string($connexio, $_POST['data_inici'] ?? '');
+    $data_fi = mysqli_real_escape_string($connexio, $_POST['data_fi'] ?? ''); // Asegúrate de usar 'data_fi' en lugar de 'data_final'
+    $id_projecte = 1; // El ID del proyecto siempre es 1
+
+    if ($id_activitat && $nom_activitat && $data_inici && $data_fi) {
+        $sql = "INSERT INTO activitat (id_projecte, data_inici, data_fi, id_activitat, nom_activitat) 
+                VALUES ('$id_projecte', '$data_inici', '$data_fi', '$id_activitat', '$nom_activitat')";
+        
+        if (mysqli_query($connexio, $sql)) {
+            $mensaje = "Activitat creada amb èxit!";
+        } else {
+            $mensaje = "Error en crear l'activitat: " . mysqli_error($connexio);
+        }
+    } else {
+        $mensaje = "Tots els camps són obligatoris!";
+    }
+}
+
+    // Modificar actividad
+    if (isset($_POST['modificar_activitat'])) {
+        $id_activitat = mysqli_real_escape_string($connexio, $_POST['id_activitat']);
+        $nom_activitat = mysqli_real_escape_string($connexio, $_POST['nom_activitat']);
+        $data_inici = mysqli_real_escape_string($connexio, $_POST['data_inici']);
+        $data_final = mysqli_real_escape_string($connexio, $_POST['data_final']);
+
+        if ($id_activitat) {
+            $sql = "UPDATE activitat SET 
+                    nom_activitat = '$nom_activitat', 
+                    data_inici = '$data_inici', 
+                    data_final = '$data_final' 
+                    WHERE id_activitat = '$id_activitat'";
+            if (mysqli_query($connexio, $sql)) {
+                $mensaje = "Activitat modificada amb èxit!";
+            } else {
+                $mensaje = "Error en modificar l'activitat: " . mysqli_error($connexio);
+            }
+        } else {
+            $mensaje = "Cal indicar l'ID de l'activitat per modificar!";
+        }
+    }
+
+    // Eliminar actividad
+    if (isset($_POST['eliminar_activitat'])) {
+        $id_activitat = mysqli_real_escape_string($connexio, $_POST['id_activitat']);
+        if ($id_activitat) {
+            $sql = "DELETE FROM activitat WHERE id_activitat = '$id_activitat'";
+            if (mysqli_query($connexio, $sql)) {
+                $mensaje = "Activitat eliminada amb èxit!";
+            } else {
+                $mensaje = "Error en eliminar l'activitat: " . mysqli_error($connexio);
+            }
+        } else {
+            $mensaje = "Cal indicar l'ID de l'activitat per eliminar!";
+        }
+    }
+}
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Panell d'Alumnes </title>
+    <title>Panell d'Activitats</title>
     <link rel="stylesheet" href="lumiere2profe.css">
     <link rel="icon" href="img/logo_lumiere-removebg-preview.png" type="image/x-icon">
 </head>
@@ -35,29 +100,33 @@ $nombreProfesor = $_SESSION['nombre_profesor'] ?? 'Desconocido'; // Si no hay se
     </div>
 
     <div class="title-container">
-        <h1 class="tituloassignatures"> Creació d'Activitats </h1>
+        <h1>Gestió d'Activitats</h1>
     </div>
 
-<br><br>
+    <br><br>
 
-    <form class="crearactivitat">
+    <form class="crearactivitat" method="POST">
         <div class="form-container">
-            <input type="text" id="nom_activitat" placeholder="Nom de l'Activitat">
+            <input type="text" name="nom_activitat" placeholder="Nom de l'Activitat" required>
             <br><br>
-            <input type="text" id="id_activitat" placeholder="ID de l'activitat">
+            <input type="text" name="id_activitat" placeholder="ID de l'activitat" required>
             <br><br>
-            <input type="datetime-local" id="data_inici">
+            <input type="datetime-local" name="data_inici" required>
             <br><br>
-            <input type="datetime-local" id="data_final">
+            <input type="datetime-local" name="data_final" required>
             <br><br><br><br>
 
             <div class="form-buttons">
-                <button type="button" class="botonform">Crear Activitat</button>
-                <button type="button" class="botonform">Modificar Activitat</button>
-                <button type="button" class="botonform">Eliminar Activitat</button>
+                <button type="submit" class="botonform" name="crear_activitat">Crear Activitat</button>
+                <button type="submit" class="botonform" name="modificar_activitat">Modificar Activitat</button>
+                <button type="submit" class="botonform" name="eliminar_activitat">Eliminar Activitat</button>
             </div>
         </div>
     </form>
+
+    <?php if ($mensaje) : ?>
+        <p style="color: green;"><?php echo $mensaje; ?></p>
+    <?php endif; ?>
 
     <script>
         // Función para ir a la página anterior
@@ -66,3 +135,5 @@ $nombreProfesor = $_SESSION['nombre_profesor'] ?? 'Desconocido'; // Si no hay se
         }
     </script>
 </body>
+
+</html>
--- gestio_professorat.php
+++ gestio_professorat.php
@@ -7,22 +7,148 @@ $bbdd = "micro2";
 $connexio = mysqli_connect($servidor, $usuari, $clau, $bbdd);
 
 // Verificar la sesión del usuario (nombre del profesor)
-// Supongamos que el nombre del profesor está almacenado en una variable de sesión llamada 'nombre_profesor'
 session_start();
 $nombreProfesor = $_SESSION['nombre_profesor'] ?? 'Desconocido'; // Si no hay sesión, mostrar 'Desconocido'
+
+// Mensaje para mostrar resultados de las acciones
+$mensaje = "";
+
+// Manejo del formulario para estudiantes
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Crear estudiante
+    if (isset($_POST['crear_estudiant'])) {
+        $id_estudiant = mysqli_real_escape_string($connexio, $_POST['id_estudiant'] ?? '');
+        $nom = mysqli_real_escape_string($connexio, $_POST['nom_estudiant'] ?? '');
+        $cognom = mysqli_real_escape_string($connexio, $_POST['cognom_estudiant'] ?? '');
+        $contrasenya = mysqli_real_escape_string($connexio, $_POST['contrasenya_estudiant'] ?? '');
+        $correu = mysqli_real_escape_string($connexio, $_POST['email_estudiant'] ?? '');
+        $estat = mysqli_real_escape_string($connexio, $_POST['estat_estudiant'] ?? 'activo');
+    
+        if ($nom && $cognom && $contrasenya && $correu) {
+            $sql = "INSERT INTO estudiant (id_estudiant,correu_estudiant, nom_estudiant, pw_estudiant, estat) 
+                    VALUES ('$id_estudiant','$correu', '$nom $cognom', '$contrasenya', '$estat')";
+            if (mysqli_query($connexio, $sql)) {
+                $mensaje = "Estudiant creat amb èxit!";
+            } else {
+                $mensaje = "Error en crear l'estudiant: " . mysqli_error($connexio);
+            }
+        } else {
+            $mensaje = "Tots els camps són obligatoris!";
+        }
+    }
+
+    // Modificar estudiante
+    if (isset($_POST['modificar_estudiant'])) {
+        $id = mysqli_real_escape_string($connexio, $_POST['id_estudiant'] ?? '');
+        $nom = mysqli_real_escape_string($connexio, $_POST['nom_estudiant'] ?? '');
+        $cognom = mysqli_real_escape_string($connexio, $_POST['cognom_estudiant'] ?? '');
+        $contrasenya = mysqli_real_escape_string($connexio, $_POST['contrasenya_estudiant'] ?? '');
+        $correu = mysqli_real_escape_string($connexio, $_POST['email_estudiant'] ?? '');
+        $estat = mysqli_real_escape_string($connexio, $_POST['estat_estudiant'] ?? '');
+
+        if ($id && ($nom || $cognom || $contrasenya || $correu || $estat)) {
+            $sql = "UPDATE estudiant SET 
+                    nom_estudiant = '$nom $cognom', 
+                    pw_estudiant = '$contrasenya', 
+                    correu_estudiant = '$correu', 
+                    estat = '$estat' 
+                    WHERE id_estudiant = '$id'";
+            if (mysqli_query($connexio, $sql)) {
+                $mensaje = "Estudiant modificat amb èxit!";
+            } else {
+                $mensaje = "Error en modificar l'estudiant: " . mysqli_error($connexio);
+            }
+        } else {
+            $mensaje = "Cal indicar l'ID de l'estudiant i almenys un camp per modificar!";
+        }
+    }
+
+    // Eliminar estudiante
+    if (isset($_POST['eliminar_estudiant'])) {
+        $id = mysqli_real_escape_string($connexio, $_POST['id_estudiant'] ?? '');
+        if ($id) {
+            $sql = "DELETE FROM estudiant WHERE id_estudiant = '$id'";
+            if (mysqli_query($connexio, $sql)) {
+                $mensaje = "Estudiant eliminat amb èxit!";
+            } else {
+                $mensaje = "Error en eliminar l'estudiant: " . mysqli_error($connexio);
+            }
+        } else {
+            $mensaje = "Cal indicar l'ID de l'estudiant per eliminar!";
+        }
+    }
+
+    // Manejo de items
+    if (isset($_POST['crear_item'])) {
+        $nom_item = mysqli_real_escape_string($connexio, $_POST['nom_item'] ?? '');
+        $tipus = mysqli_real_escape_string($connexio, $_POST['tipus'] ?? '');
+        $percentatge = mysqli_real_escape_string($connexio, $_POST['percentatge'] ?? '');
+        $id_activitat = mysqli_real_escape_string($connexio, $_POST['id_activitat'] ?? '');
+
+        if ($nom_item && $tipus && $percentatge && $id_activitat) {
+            $sql = "INSERT INTO item (nom_item, tipus_item, percentatge, id_activitat) 
+                    VALUES ('$nom_item', '$tipus', '$percentatge', '$id_activitat')";
+            if (mysqli_query($connexio, $sql)) {
+                $mensaje = "Item creat amb èxit!";
+            } else {
+                $mensaje = "Error en crear l'item: " . mysqli_error($connexio);
+            }
+        } else {
+            $mensaje = "Tots els camps són obligatoris per crear un item!";
+        }
+    }
+
+    // Modificar item
+    if (isset($_POST['modificar_item'])) {
+        $id_item = mysqli_real_escape_string($connexio, $_POST['id_item'] ?? '');
+        $nom_item = mysqli_real_escape_string($connexio, $_POST['nom_item'] ?? '');
+        $tipus = mysqli_real_escape_string($connexio, $_POST['tipus'] ?? '');
+        $percentatge = mysqli_real_escape_string($connexio, $_POST['percentatge'] ?? '');
+        $id_activitat = mysqli_real_escape_string($connexio, $_POST['id_activitat'] ?? '');
+
+        if ($id_item) {
+            $sql = "UPDATE item SET 
+                    nom_item = '$nom_item', 
+                    tipus_item = '$tipus', 
+                    percentatge = '$percentatge', 
+                    id_activitat = '$id_activitat' 
+                    WHERE id_item = '$id_item'";
+            if (mysqli_query($connexio, $sql)) {
+                $mensaje = "Item modificat amb èxit!";
+            } else {
+                $mensaje = "Error en modificar l'item: " . mysqli_error($connexio);
+            }
+        } else {
+            $mensaje = "Cal indicar l'ID de l'item per modificar!";
+        }
+    }
+
+    // Eliminar item
+    if (isset($_POST['eliminar_item'])) {
+        $id_item = mysqli_real_escape_string($connexio, $_POST['id_item'] ?? '');
+        if ($id_item) {
+            $sql = "DELETE FROM item WHERE id_item = '$id_item'";
+            if (mysqli_query($connexio, $sql)) {
+                $mensaje = "Item eliminat amb èxit!";
+            } else {
+                $mensaje = "Error en eliminar l'item: " . mysqli_error($connexio);
+            }
+        } else {
+            $mensaje = "Cal indicar l'ID de l'item per eliminar!";
+        }
+    }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Panell d'Alumnes </title>
+    <title>Panell d'Alumnes</title>
     <link rel="stylesheet" href="lumiere2profe.css">
     <link rel="icon" href="img/logo_lumiere-removebg-preview.png" type="image/x-icon">
 </head>
-
 <body>
     <div class="header">
         <div class="welcome-message">
@@ -35,40 +161,42 @@ $nombreProfesor = $_SESSION['nombre_profesor'] ?? 'Desconocido'; // Si no hay se
     </div>
 
     <div class="title-container">
-        <h1 class="tituloassignatures"> Gestió del Professorat </h1>
+        <h1 class="tituloassignatures">Gestió del Professorat</h1>
     </div>
 
     <div class="columna">
         <div>
             <h1>Estudiants</h1>
             <br><br>
-            <form class="formgestioprofe">
-                <input type="text" id="nom_estudiant" placeholder=" Ingresa el teu nom ">
+            <?php if ($mensaje) : ?>
+                <p style="color: green;"><?php echo $mensaje; ?></p>
+            <?php endif; ?>
+            <form class="formgestioprofe" method="POST">
+                <input type="text" name="id_estudiant" placeholder="ID de l'Estudiant (Per Modificar o Eliminar)">
                 <br><br>
-                <input type="text" id="cognom_estudiant" placeholder=" Ingresa primer cognom ">
+                <input type="text" name="nom_estudiant" placeholder="Nom">
                 <br><br>
-                <input type="password" id="contrasenya_estudiant" placeholder=" Contrasenya ">
+                <input type="text" name="cognom_estudiant" placeholder="Cognom">
                 <br><br>
-                <input type="text" id="email_estudiant" placeholder=" Correu Electrònic ">
+                <input type="password" name="contrasenya_estudiant" placeholder="Contrasenya">
                 <br><br>
-                <input type="text" id="estat_estudiant" placeholder=" Estat Estudiant ">
+                <input type="text" name="email_estudiant" placeholder="Correu Electrònic">
                 <br><br>
+                <input type="text" name="estat_estudiant" placeholder="Estat" value="actiu">
                 <br><br>
-
-                <button type="button" class="botonform" onclick=""> Crear Estudiant </button>
-                <button type="button" class="botonform" onclick=""> Modificar Estudiant </button>
-                <button type="button" class="botonform" onclick=""> Eliminar Estudiant </button>
+                <button type="submit" class="botonform" name="crear_estudiant">Crear Estudiant</button>
+                <button type="submit" class="botonform" name="modificar_estudiant">Modificar Estudiant</button>
+                <button type="submit" class="botonform" name="eliminar_estudiant">Eliminar Estudiant</button>
             </form>
         </div>
 
         <div>
             <h1>Items</h1>
             <br><br>
-            <form class="formgestioprofe">
-                <input type="text" id="nom" placeholder="Nom del ítem">
+            <form class="formgestioprofe" method="POST">
+                <input type="text" name="nom_item" placeholder="Nom de l'Item">
                 <br><br>
-
-                <label for="tipus">Tipus del ítem:</label>
+                <label for="tipus">Tipus de l'Item:</label>
                 <br>
                 <input type="radio" id="soft-skills" name="tipus" value="Soft Skills">
                 <label for="soft-skills">Soft Skills</label>
@@ -76,29 +204,21 @@ $nombreProfesor = $_SESSION['nombre_profesor'] ?? 'Desconocido'; // Si no hay se
                 <input type="radio" id="hard-skills" name="tipus" value="Hard Skills">
                 <label for="hard-skills">Hard Skills</label>
                 <br><br>
-                <input type="text" id="percentatge" placeholder=" Percentatge ">
+                <input type="text" name="percentatge" placeholder="Percentatge">
                 <br><br>
-                <input type="text" id="id_activitat" placeholder=" Id_Activitat ">
+                <input type="text" name="id_activitat" placeholder="ID Activitat">
                 <br><br>
-
-
-                <br><br>
-
-
-                <button type="button" class="botonform" onclick=""> Crear Item </button>
-                <button type="button" class="botonform" onclick=""> Modificar Item </button>
-                <button type="button" class="botonform" onclick=""> Eliminar Item </button>
+                <button type="submit" class="botonform" name="crear_item">Crear Item</button>
+                <button type="submit" class="botonform" name="modificar_item">Modificar Item</button>
+                <button type="submit" class="botonform" name="eliminar_item">Eliminar Item</button>
             </form>
-
         </div>
-
     </div>
 
     <script>
-        // Función para ir a la página anterior
         function goBack() {
             window.history.back();
         }
     </script>
-
 </body>
+</html>
